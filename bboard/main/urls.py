4ac3514d2a153d219dbacea784e2d893127e17de
Snezhana-star <cnega.2002@gmail.com>
2022-09-15 13:45:49 +0700

    Merge remote-tracking branch 'origin/master'

--- bboard/main/urls.py
+++ bboard/main/urls.py
@@ -8,6 +8,7 @@ from .views import ChangeUserInfoView
 from .views import BBPasswordChangeView
 from .views import RegisterUserView, RegisterDoneView
 from .views import user_activate
+from .views import DeleteUserView
 
 app_name = 'main'
 
@@ -16,6 +17,7 @@ urlpatterns = [
    path('accounts/login', BBLoginView.as_view(), name='login'),
    path('accounts/profile/', profile, name='profile'),
    path('accounts/logout/', BBLogoutView.as_view(), name='logout'),
+   path('accounts/profile/delete/', DeleteUserView.as_view(), name='profile_delete'),
    path('accounts/profile/change/', ChangeUserInfoView.as_view(), name='profile_change'),
    path('accounts/profile/', profile, name='profile'),
    path('accounts/password/change/', BBPasswordChangeView.as_view(), name='password_change'),
--- bboard/main/views.py
+++ bboard/main/views.py
@@ -9,15 +9,16 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
-from django.views.generic import UpdateView
 from django.contrib.auth.views import PasswordChangeView
-from django.views.generic import CreateView
 from .forms import RegisterUserForm
 from django.views.generic.base import TemplateView
 from .forms import ChangeUserInfoForm
 from .models import AdvUser
 from django.core.signing import BadSignature
 from .utilities import signer
+from django.views.generic import UpdateView, CreateView, DeleteView
+from django.contrib.auth import logout
+from django.contrib import messages
 
 def index(request):
    return render(request, 'main/index.html')
@@ -86,3 +87,21 @@ def user_activate(request, sign):
        user.save()
    return render(request, template)
 
+class DeleteUserView(LoginRequiredMixin, DeleteView):
+   model = AdvUser
+   template_name = 'main/delete_user.html'
+   success_url = reverse_lazy('main:index')
+
+   def dispatch(self, request, *args, **kwargs):
+       self.user_id = request.user.pk
+       return super().dispatch(request, *args, **kwargs)
+
+   def post(self, request, *args, **kwargs):
+       logout(request)
+       messages.add_message(request, messages.SUCCESS, 'Пользователь удален')
+       return super().post(request, *args, **kwargs)
+
+   def get_object(self, queryset=None):
+       if not queryset:
+           queryset = self.get_queryset()
+       return get_object_or_404(queryset, pk=self.user_id)
