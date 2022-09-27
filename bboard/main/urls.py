90365f358cc6c6bc0e86559264171c7b694f40f1
Snezhana-star <cnega.2002@gmail.com>
2022-09-27 14:10:37 +0700

    Merge remote-tracking branch 'origin/master'

--- bboard/main/urls.py
+++ bboard/main/urls.py
@@ -12,7 +12,7 @@ from .views import DeleteUserView
 from .views import by_rubric
 from .views import detail
 from .views import profile_bb_add
-from .views import profile_bb_delete
+from .views import profile_bb_delete, profile_bb_change
 
 
 app_name = 'main'
@@ -26,6 +26,8 @@ urlpatterns = [
     path('accounts/profile/change/', ChangeUserInfoView.as_view(), name='profile_change'),
     path('accounts/password/change/', BBPasswordChangeView.as_view(), name='password_change'),
     path('accounts/profile/add/', profile_bb_add, name='profile_bb_add'),
+    path('accounts/profile/change/<int:pk>/', profile_bb_change, name='profile_bb_change'),
+    path('accounts/profile/delete/<int:pk>/', profile_bb_delete, name='profile_bb_delete'),
     path('accounts/profile/', profile, name='profile'),
     path('accounts/login', BBLoginView.as_view(), name='login'),
     path('<int:rubric_pk>/<int:pk>/', detail, name='detail'),
